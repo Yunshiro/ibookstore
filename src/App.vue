2ca3d90b1d441e261c7c42f2577fdae3fed5827e
Yunshiro <cookiemakefun@qq.com>
2024-11-12 00:46:47 +0800

    first commit.

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Clock from './components/Clock.vue'
+</script>
+
+<template>
+
+  <Clock />
+  
+</template>
+
+<style scoped>
+.btn {
+  width: 100px;
+  height: 50px;
+
+  color: #987ceb;
+  border: 10px;
+}
+</style>
---
+++ src/components/Clock.vue
@@ -0,0 +1,38 @@
+
+
+<script lang="js" setup>
+import { onMounted, ref } from 'vue'
+
+// currentTime show the time in templates.
+let currentTime = ref("")
+
+// get current time
+function getCurrentTime() {
+  const currentDate = new Date()
+
+  // get hours, minus, seconds
+  const hours = currentDate.getHours()
+  const minus = currentDate.getMinutes()
+  const seconds = currentDate.getSeconds()
+
+  // time format
+  currentTime.value = `${hours}:${minus}:${seconds}`
+}
+
+// get time at once on Mounted
+onMounted(() => {
+  getCurrentTime()
+})
+
+setInterval(getCurrentTime, 1000)
+
+</script>
+
+<template>
+  <!-- show the time -->
+  <h1>Time Now:{{ currentTime }}</h1>
+</template>
+
+<style scoped>
+
+</style>
